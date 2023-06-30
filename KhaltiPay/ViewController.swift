016f62b0ea33182e406a87557cf8e430882cddc1
Sange Sherpa <sherpasange1215@gmail.com>
2023-06-30 10:53:38 +0545

    Initial Commit

---
+++ KhaltiPay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KhaltiPay
+//
+//  Created by Sange Sherpa on 30/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
